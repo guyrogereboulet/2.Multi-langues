fa0f7e306c364ce3979de1638efae4dc400f6456
Guy Roger Eboulet <guy_eboulet_reply@whirlpool.com>
2022-02-04 11:21:22 +0100

    Create Context

--- src/Components/Contenu/Contenu.js
+++ src/Components/Contenu/Contenu.js
@@ -1,8 +1,11 @@
 import React, { useContext } from "react";
 import "./Contenu.css";
 import data from "../../assets/data";
+import { Context } from "../../context/langContext";
 
 const Contenu = () => {
+  const { lang } = useContext(Context);
+  console.log(lang);
   return (
     <div className="content">
       <h1 className="title">{data.FR.title}</h1>
