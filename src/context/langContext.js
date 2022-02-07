ba4250f3535d44c958fbec962f43c423d4a680df
Guy Roger Eboulet <guy_eboulet_reply@whirlpool.com>
2022-02-07 08:04:09 +0100

    Detect the language of browser

--- src/context/langContext.js
+++ src/context/langContext.js
@@ -1,8 +1,16 @@
 import React, { createContext, useState } from "react";
 export const Context = createContext();
 
+const supportedLangs = ["EN", "FR", "ES"];
+let browserLang = navigator.language.slice(0, 2).toUpperCase();
+
+if (supportedLangs.indexOf(browserLang) === -1) {
+  console.log("not supported");
+  browserLang = "EN";
+}
+
 const ContextProvider = (props) => {
-  const [lang, setLang] = useState("FR");
+  const [lang, setLang] = useState(browserLang);
   const toggleLang = (changeLang) => {
     setLang(changeLang);
   };
